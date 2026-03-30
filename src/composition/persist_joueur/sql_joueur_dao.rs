6c4a736faad934cb7e6e1da18ed522bce8f2332f
YoanBst <yoan.bastides@etu.umontpellier.fr>
2026-03-30 16:05:59 +0200

    Revert "change logic for getting a player"
    This reverts commit 2ab8abf7f7740366220fe01a6b9d30c3e9de5067.

--- src/composition/persist_joueur/sql_joueur_dao.rs
+++ src/composition/persist_joueur/sql_joueur_dao.rs
@@ -10,35 +10,30 @@ pub struct SqliteJoueurDAO {
 impl JoueurDAO for SqliteJoueurDAO {
     fn get_joueurs_by_club_id(&self, club_id: i32) -> Result<Vec<Joueur>> {
         let mut stmt = self.conn.prepare(
-           "
-        SELECT
-            j.id,
-            j.nom,
-            j.age,
-            j.poste,
-            j.reputation,
-            j.valeur_marche_eur,
-            j.salaire_semaine_eur,
-            c.nom AS club_nom,
-            COALESCE(aj.note_actuelle, ag.note_actuelle) AS note_actuelle,
-            COALESCE(aj.forme, ag.forme) AS forme,
-            COALESCE(aj.nationalite, ag.nationalite) AS nationalite
-        FROM joueurs j
-        JOIN clubs c ON j.club_id = c.id
-        LEFT JOIN attributs_joueurs aj ON aj.joueur_id = j.id
-        LEFT JOIN attributs_gardiens ag ON ag.joueur_id = j.id
-        WHERE j.club_id = ?1
-        ORDER BY
-            CASE j.poste
-                WHEN 'GARDIEN' THEN 1
-                WHEN 'DEFENSE' THEN 2
-                WHEN 'MILIEU' THEN 3
-                WHEN 'ATTAQUE' THEN 4
-                ELSE 5
-            END,
-            j.reputation DESC
-        "
-    )?;
+            "
+            SELECT
+                j.id,
+                j.nom,
+                j.age,
+                j.poste,
+                j.reputation,
+                j.valeur_marche_eur,
+                j.salaire_semaine_eur,
+                c.nom
+            FROM joueurs j
+            JOIN clubs c ON j.club_id = c.id
+            WHERE j.club_id = ?
+            ORDER BY
+                CASE j.poste
+                    WHEN 'GARDIEN' THEN 1
+                    WHEN 'DEFENSE' THEN 2
+                    WHEN 'MILIEU' THEN 3
+                    WHEN 'ATTAQUE' THEN 4
+                    ELSE 5
+                END,
+                j.reputation DESC
+            "
+        )?;
 
         let joueur_iter = stmt.query_map([club_id], |row| {
             Ok(Joueur {
